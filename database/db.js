fd282c22a3860e243582283a266354d67042fab1
Thi0105 <dinhnhatthi0105@gmail.com>
2026-06-05 18:00:42 +0300

    Connect app to remote database

--- database/db.js
+++ database/db.js
@@ -2,11 +2,15 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 const db = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_DATABASE,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 })
 
 const connectDB = async () => {

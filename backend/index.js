c0c1ed04819c3213c359f5ea6bccba3e85e5b089
Thi0105 <dinhnhatthi0105@gmail.com>
2026-06-08 00:10:17 +0300

    Update backend to serve subfolders

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ app.use(cors())
 
 app.use('/api/auth', authRoute)
 
-app.use(express.static(path.join(__dirname,'../frontend/html')))
+app.use(express.static(path.join(__dirname,'../frontend')))
+app.use(express.static(path.join(__dirname, '../frontend/html')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/html/index.html'))
